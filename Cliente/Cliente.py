5d8ed0f3bc95fa04de17056f0fb7bb91d1b75a7e
David <145496969+GarciByte@users.noreply.github.com>
2025-01-25 19:59:52 +0100

    Actualizar proyecto

--- Cliente/Cliente.py
+++ Cliente/Cliente.py
@@ -1,42 +1,75 @@
-import socket, threading
+import socket
+import threading
+import json
 
-#     A B C D E F G H I J 
-# 1                      
-# 2                      
-# 3                       
-# 4                       
-# 5                       
-# 6                       
-# 7                       
-# 8                      
-# 9                      
-# 10
+########################
+#   0 1 2 3 4 5 6 7 8 9
+# A
+# B
+# C
+# D
+# E
+# F
+# G
+# H
+# I
+# J
 
+nickname = input("Elige tu apodo: ")
+host = "127.0.0.1"
+port = 7976
 
-nickname = input( "Choose your nickname: " )
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(( '127.0.0.1' , 7976 ))
+client.connect((host, port))
 
 
-def receive () :
-    while True :
-        try :
-            message = client.recv( 1024 ).decode( 'ascii' )
-            if message == 'NICKNAME' :
-                client.send(nickname.encode( 'ascii' ))
-            else :
-                print(message)
-        except :
-            print( "An error occured!" )
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode("utf-8")
+            message_data = json.loads(message)
+
+            if "type" in message_data:
+
+                # Apodo
+                if (message_data["type"] == "request" and message_data["message"] == "NICKNAME"):
+                    client.send(json.dumps({
+                        "type": "nickname", 
+                        "message": nickname
+                        }).encode("utf-8"))
+
+                # Notificación
+                elif message_data["type"] == "notification":
+                    print(message_data["message"])
+
+                # Acción
+                elif message_data["type"] == "action":
+
+                    # Colocar barcos
+                    if message_data["action"] == "place_ships":
+                        place_ships()
+
+        except:
+            print("Se ha producido un error.")
             client.close()
             break
 
-def write () :
-    while True :
-        message = '{}: {}' .format(nickname, input( '' ))
-        client.send(message.encode( 'ascii' ))
+
+def place_ships():
+    barcos = input("Barcos: ")
+
+    ships = {
+        "Barco 1": ["A1", "A2", "A3", "A4", "A5"],
+        "Barco 2": ["B1", "B2", "B3", "B4"],
+        "Barco 3": ["C1", "C2", "C3"],
+        "Barco 4": ["D1", "D2", "D3"],
+        "Barco 5": ["E1", "E2"],
+    }
+
+    message_data = {"player": nickname, "ships": ships}
+
+    client.send(json.dumps(message_data).encode("utf-8"))
+
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
-write_thread = threading.Thread(target=write)
-write_thread.start()
--- Servidor/Servidor.py
+++ Servidor/Servidor.py
@@ -1,53 +1,116 @@
-import socket, threading
+import socket
+import threading
+import json
 
-host = '127.0.0.1'
+host = "127.0.0.1"
 port = 7976
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server.bind((host, port))
+server.listen(2)
 
-
-server.listen()
 clients = []
 nicknames = []
+players_ships = []
 
-primerMensaje = {'status':'connected', 'message': 'Esperando otro jugador...'}
+game_started = False
+
+first_player = None
+first_player_nickname = None
+first_player_ships = None
+
+second_player = None
+second_player_nickname = None
+second_player_ships = None
 
 
-def broadcast (message) :
-    for client in clients: client.send(message)
+def handle(client):
+    global first_player, first_player_nickname, first_player_ships
+    global second_player, second_player_nickname, second_player_ships
 
-def handle (client) :
-    while True :
-        try :
-            message = client.recv( 1024 )
-            broadcast(message)
-        except :
+    while True:
+        try:
+            message = client.recv(1024).decode("utf-8")
+            message_data = json.loads(message)
+
+            if "ships" in message_data:
+                print(f"Barcos del jugador {message_data['player']} obtenidos.")
+                players_ships.append({
+                    "player": message_data["player"],
+                    "ships": message_data["ships"],
+                })
+
+                if len(players_ships) == 2:
+                    print("Los barcos de ambos jugadores se han obtenido.")
+                    first_player = players_ships[0]
+                    first_player_nickname = first_player["player"]
+                    first_player_ships = first_player["ships"]
+
+                    second_player = players_ships[1]
+                    second_player_nickname = second_player["player"]
+                    second_player_ships = second_player["ships"]
+
+                    print(f"Barcos del jugador {first_player_nickname}:")
+                    for barco, posiciones in first_player_ships.items():
+                        print(f"{barco}: {posiciones}")
+
+                    print(f"Barcos del jugador {second_player_nickname}:")
+                    for barco, posiciones in second_player_ships.items():
+                        print(f"{barco}: {posiciones}")
+
+        except:
+            print("Se ha producido un error.")
             index = clients.index(client)
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
-            broadcast( '{} left!' .format(nickname).encode( 'ascii' ))
             nicknames.remove(nickname)
             break
 
 
-def receive () :
-    while True :
-        client, address = server.accept()
-        print( "Connected with {}" .format(str(address)))
-        client.send( 'NICKNAME' .encode( 'ascii' ))
-        nickname = client.recv( 1024 ).decode( 'ascii' )
+def receive():
+    print(f"Servidor iniciado en {host}:{port}")
+    global game_started
 
-        nicknames.append(nickname)
-        clients.append(client)
+    while True:
+        if len(clients) < 2:
+            client, address = server.accept()
+            print(f"Conexión establecida con {address}")
 
-        print( "Nickname is {}" .format(nickname))
-        broadcast( "{} joined!" .format(nickname).encode( 'ascii' ))
-        client.send( 'Connected to server!' .encode( 'ascii' ))
+            # Apodo
+            client.send(json.dumps({"type": "request", "message": "NICKNAME"}).encode("utf-8"))
+            nickname_data = json.loads(client.recv(1024).decode("utf-8"))
 
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
+            if nickname_data["type"] == "nickname":
+                nickname = nickname_data["message"]
+                nicknames.append(nickname)
+                clients.append(client)
+                print(f"Jugador {nickname} conectado.")
+
+                client.send(json.dumps({
+                    "type": "notification",
+                    "message": f"¡Bienvenido al servidor {nickname}! Esperando otro jugador...",
+                }).encode("utf-8"))
+
+                thread = threading.Thread(target=handle, args=(client,))
+                thread.start()
+
+        if len(clients) == 2 and not game_started:
+            print("Hay 2 jugadores, el juego va a comenzar.")
+            
+            for client in clients:
+                
+                client.send(json.dumps({
+                    "type": "notification",
+                    "message": "El juego ha comenzado. Prepárate para colocar tus barcos.",
+                }).encode("utf-8"))
+
+                client.send(json.dumps({
+                    "type": "action",
+                    "action": "place_ships"
+                }).encode("utf-8"))
+                
+                game_started = True
+
 
 receive()
